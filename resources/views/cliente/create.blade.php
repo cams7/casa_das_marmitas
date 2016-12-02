59f2ebd0b59c0568ed035e140107863ae5072e7c
César A. M <ceanma@gmail.com>
2016-12-02 17:50:58 +0000

    Foram incluidas 'Forms & HTML Class'

--- resources/views/cliente/create.blade.php
+++ resources/views/cliente/create.blade.php
@@ -11,5 +11,24 @@
 
 @section('content')
 	<h1>Cadastra novo cliente</h1>
-	
+
+	<?php
+  	echo Form::open(['url' => 'foo/bar']);
+    echo Form::text('username','username');
+    echo '<br/>';
+    echo Form::text('email','yourmail@here.com');
+    echo '<br/>';
+    echo Form::password('password');
+    echo '<br/>';
+    echo Form::checkbox('name','value');
+    echo '<br/>';
+    echo Form::radio('name','value');
+    echo '<br/>';
+    echo Form::file('image');
+    echo '<br/>';
+    echo Form::select('Gender',['P'=>'perempuan','L'=> 'Laki-laki']);
+    echo '<br/>';
+    echo Form::submit('Register Now');
+  	echo Form::close(); 
+  	?>
 @endsection
