5a6caadbbe22fdc41af9164084c5172d8bc15763
César A. M <ceanma@gmail.com>
2016-12-02 13:16:28 +0000

    Foi removida a funcionalidade 'migrations-generator'

--- config/app.php
+++ config/app.php
@@ -162,9 +162,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Way\Generators\GeneratorsServiceProvider::class,
-        Xethron\MigrationsGenerator\MigrationsGeneratorServiceProvider::class,
-
+        
         /*
          * Package Service Providers...
          */
